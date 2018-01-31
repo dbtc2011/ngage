afa0428613be638ea3aea68cce2c45a522e7b582
Mary Marielle Miranda <mary.miranda@bren-inc.com>
2018-01-31 19:50:25 +0800

    Added Core Data implementation and Manager class

---
+++ Ngage/Model/Mission.swift
@@ -0,0 +1,30 @@
+//
+//  Mission.swift
+//  Ngage
+//
+//  Created by Mary Marielle Miranda on 30/01/2018.
+//  Copyright © 2018 Mark Louie Angeles. All rights reserved.
+//
+
+import UIKit
+
+struct Mission {
+    var code = ""
+    var userId = ""
+    
+    var brand = ""
+    var colorBackground = ""
+    var colorPrimary = ""
+    var colorSecondary = ""
+    var createdBy = ""
+    var endDate = ""
+    var imageUrl = ""
+    var isClaimed = false
+    var pointsRequiredToUnlock = ""
+    var reward = ""
+    var rewardDetails = ""
+    var rewardInfo = ""
+    var rewardType = ""
+    var startDate = ""
+    var title = ""
+}
