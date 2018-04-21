1146e116d61809a449f3a9f5c4d224a6d311628b
marielle <mark13_06@yahoo.com>
2018-04-21 17:22:50 +0800

    Worked on Notifications page

--- Ngage/View/NotificationViewController.swift
+++ Ngage/View/NotificationViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+enum NotifcationType: String {
+    case UP = "UP", //Blast All - New Version with Button Update
+         I = "I", //Blast All - with Invite Button
+         Default
+}
 
+struct NotificationModel {
+    var id: NotifcationType!
+    var title = ""
+    var message = ""
+    var date = ""
+}
+
+class NotificationViewController: MainViewController {
+
+    //MARK: - Properties
+    
+    @IBOutlet weak var tblNotifications: UITableView!
+    @IBOutlet weak var imgNoData: UIImageView!
+    
+    private var notifications = [NotificationModel]()
+    
+    //MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupMockData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         navigationController?.navigationBar.barTintColor = UIColor(red: 39.0/255.0, green: 120.0/255.0, blue: 206.0/255.0, alpha: 1)
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
@@ -28,24 +51,57 @@ class NotificationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: - Methods
+    
+    private func setupMockData() {
+        let notificationUpdate = NotificationModel(id: .UP, title: "Update Available",
+                                                  message: "We will be giving 10 points for each referral so go and tell your friends about Ngage! Thank you!", date: "31/1/2018")
+        let notificationInvite = NotificationModel(id: .I, title: "Invite",
+                                                  message: "Earn more points by inviting your friends to install Ngage and make sure to give them your referral code.", date: "21/4/2018")
+        let notificationDefault = NotificationModel(id: .Default, title: "Announcement",
+                                                  message: "This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement. This is just a test default announcement.", date: "01/05/2018")
+        notifications = [notificationUpdate, notificationInvite, notificationDefault]
+        
+        tblNotifications.reloadData()
+        
+        if notifications.count == 0 {
+            imgNoData.isHidden = false
+        }
+    }
+    
+    //MARK: - IBAction Delegate
+    
     @IBAction func backButtonClicked(_ sender: UIBarButtonItem) {
         let transition = CATransition()
         transition.duration = 0.25
         transition.type = kCATransitionPush
         transition.subtype = kCATransitionFromLeft
         view.window!.layer.add(transition, forKey: kCATransition)
+        
         dismiss(animated: false, completion: nil)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension NotificationViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifications.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let notificationCell = tableView.dequeueReusableCell(withIdentifier: "notificationCell", for: indexPath) as! NotificationTableViewCell
+        let notification = notifications[indexPath.row]
+        notificationCell.setupCell(withNotificationModel: notification)
+        
+        return notificationCell
+    }
+}
 
+extension NotificationViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
---
+++ Ngage/View/Subclass/NotificationTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationTableViewCell.swift
+//  Ngage PH
+//
+//  Created by cybilltek on 21/04/2018.
+//  Copyright © 2018 Mark Louie Angeles. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    //MARK: - Properties
+    
+    @IBOutlet weak var lblDate: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    //MARK: - View Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    //MARK: - Methods
+    
+    func setupCell(withNotificationModel notification: NotificationModel) {
+        lblTitle.text = notification.title
+        lblMessage.text = notification.message
+        lblDate.text = notification.date
+    }
+    
+}
